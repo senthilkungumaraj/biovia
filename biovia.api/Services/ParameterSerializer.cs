9b6964b8f97811e2900fc33959249e813858281b
Kungumaraj Nachimuthu <senthilkungumaraj@gmail.com>
2018-11-26 22:15:59 -0800

    Restructured the folders

---
+++ biovia.api/Services/ParameterSerializer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace biovia.api.Services
+{
+    public static class ParameterSerializer
+    {
+        public static IList<Dictionary<string, object>> ToDictionary<TSourceType>(IList<TSourceType> source)
+        {
+            var settings = new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore
+            };
+
+            string json = JsonConvert.SerializeObject(source, settings);
+
+            return JsonConvert.DeserializeObject<IList<Dictionary<string, object>>>(json, new CustomDictionaryConverter());
+        }
+    }
+}
